6e52001ac56a61f0ef21b59f4e99c39eb7f0f8e6
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-28 19:18:17 +0500

    string and template literals

---
+++ string/app.js
@@ -0,0 +1,12 @@
+'use strict';
+let name = "Asilbek"
+console.log(name)
+let surName = "Karomatov"
+console.log(surName)
+
+let userName = "Asilbek2706"
+let message = "Hello" + " " + userName + "!"
+console.log(message)
+
+let message2 = `Hello ${userName} !`
+console.log(message2)
