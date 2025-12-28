59592dde3d8638c9cae0c913928d8f14a22f34f4
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-28 18:16:50 +0500

    data types js

---
+++ dataTypes/primitive.js
@@ -0,0 +1,36 @@
+// Number
+let age = 20
+let price = 19.99
+console.log(age)
+console.log(price)
+
+// String
+let firstName = "Asilbek"
+let lastName = "Karomatov"
+console.log(firstName)
+console.log(lastName)
+
+// Boolean
+let isUser = true
+console.log(isUser)
+
+// Undefined
+let student
+console.log(student)
+
+// Null
+let school = null
+
+// Symbol
+let id = Symbol('id')
+console.log(id)
+
+// BigInt
+const alsoHuge = BigInt(9007199254740991)
+console.log(alsoHuge)
+
+// typeof operator
+let age2 = 20
+let name = "Asilbek"
+console.log(typeof age2)
+console.log(typeof name)
