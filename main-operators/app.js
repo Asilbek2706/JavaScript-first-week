f9340c5993712ff76086594961ac994cd1e785b9
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-28 19:02:41 +0500

    Main operators

---
+++ main-operators/app.js
@@ -0,0 +1,53 @@
+// Arithmetic operators
+let a = 5, b = 10
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a++)
+console.log(a)
+console.log(++a)
+console.log(a)
+console.log(b--)
+console.log(b)
+console.log(--b)
+console.log(b)
+
+// Comparison operators
+let c = 5, d = 10, e = "5"
+const z = c
+
+console.log(c == e)
+console.log(c ===e)
+console.log(c == z)
+console.log(c === z)
+console.log(c != d)
+console.log(c != z)
+console.log(c > d)
+console.log(c < d)
+console.log(c >= d)
+console.log(c <= d) 
+
+// Assignment operators
+let f = 5, g = 10, h = 15
+
+console.log(f)
+f = g
+
+console.log(f)
+f += g
+
+console.log(f)
+f -= g
+
+console.log(g)
+h *= g
+
+console.log(h)
+h /= 5
+
+console.log(h)
+g %= 2
+
+console.log(g)
