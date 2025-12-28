8915443d898cec8ea8acf2244065f03ac9237423
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-28 18:35:24 +0500

    let, const and var in  js

---
+++ let_const_var/script.js
@@ -0,0 +1,23 @@
+'use strict'
+// let
+/* Used when declaring variables that you can modify yourself */
+let age = 19
+console.log(age)
+
+age = 20
+console.log(age)
+
+// const - means 'constant'
+/* Used to declare variables that do not change during program execution */
+/* const must always have an initial value! */
+const id = "AA0"
+id = "AA1" //❌
+
+// var
+/* var also performs the same function as let, but it is preferable to use let instead of var */
+var name = "Asilbek"
+console.log(name)
+
+// 'use strict' can be used to avoid errors in the code
+userName = "Asiloke"
+console.log(userName)
